bcb059b6bd86f75b5b0fe91f457b2acd243c04b1
rinat <r.bashirov@zenlabs.biz>
2021-12-01 22:06:47 +0500

    some fixes with linting

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
+// eslint-disable-next-line import/prefer-default-export
 export const userName = (readlineSync.question('May I have your name?: '));
