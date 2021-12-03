c3c535116e1c9d9541f300368d75450a767363fe
rinat <r.bashirov@zenlabs.biz>
2021-12-03 19:50:21 +0500

    fix logic after changes with username function

--- games/even-game.js
+++ games/even-game.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
+import userName from '../src/cli.js';
 
 const evenGame = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const getRandomInt = (min, max) => {
