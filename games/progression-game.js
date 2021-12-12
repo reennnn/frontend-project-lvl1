b6f92b607a1f40d93244ddd723153b2c40beaa4a
rinat <r.bashirov@zenlabs.biz>
2021-12-12 16:15:02 +0500

    some fixes with new game engine

--- games/progression-game.js
+++ games/progression-game.js
@@ -1,36 +1,28 @@
-import readlineSync from 'readline-sync';
-import userName from '../src/cli.js';
-import { getRandomInt } from '../src/index.js';
+import { getRandomInt, runGame } from '../src/index.js';
+
+const createProgression = (progressionLenght, progressionIncrease) => {
+  const progressionArr = [];
+  let firstElement = getRandomInt(1, 10);
+  for (let x = 0; x < progressionLenght; x += 1) {
+    progressionArr.push(firstElement += progressionIncrease);
+  }
+  return progressionArr;
+};
+
+const round = () => {
+  const progressionLenght = getRandomInt(5, 10);
+  const progressionIncrease = getRandomInt(1, 10);
+  const randomElement = getRandomInt(0, progressionLenght - 1);
+  const progressionArray = createProgression(progressionLenght, progressionIncrease);
+  const correctAnswer = progressionArray[randomElement];
+  progressionArray[randomElement] = '..';
+  const question = `Question: ${progressionArray.join(' ')}`;
+  return [correctAnswer, question];
+};
 
 const progressionGame = () => {
-  console.log(`Hello, ${userName}!`);
-  console.log('What number is missing in the progression?');
-  for (let i = 1; i <= 3; i += 1) {
-    const createProgression = (progressionLenght, progressionIncrease) => {
-      const progressionArr = [];
-      let firstElement = getRandomInt(1, 10);
-      for (let x = 0; x < progressionLenght; x += 1) {
-        progressionArr.push(firstElement += progressionIncrease);
-      }
-      return progressionArr;
-    };
-    const progressionLenght = getRandomInt(5, 10);
-    const progressionIncrease = getRandomInt(1, 10);
-    const progression = createProgression(progressionLenght, progressionIncrease);
-    const randomElement = getRandomInt(0, progression.length - 1);
-    const correctAnswer = progression[randomElement];
-    progression[randomElement] = '..';
-    console.log(`Question: ${progression.join(' ')}`);
-    const playerAnswer = readlineSync.question('Your answer: ');
-    if (playerAnswer === correctAnswer.toString()) {
-      console.log('Correct!');
-    } else {
-      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+  const task = 'What number is missing in the progression?';
+  runGame(round, task);
 };
 
 export default progressionGame;
