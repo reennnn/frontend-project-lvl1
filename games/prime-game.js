3d9b8797638c4d71902779365e21d1a562f3d829
rinat <r.bashirov@zenlabs.biz>
2021-12-12 15:51:39 +0500

    fix for new game engine

--- games/prime-game.js
+++ games/prime-game.js
@@ -1,36 +1,25 @@
-import readlineSync from 'readline-sync';
-import userName from '../src/cli.js';
-import { getRandomInt } from '../src/index.js';
+import { getRandomInt, runGame } from '../src/index.js';
 
-const primeGame = () => {
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 1; i <= 3; i += 1) {
-    const randomNumber = getRandomInt(1, 99);
-    console.log(`Question: ${randomNumber}`);
-    const playerAnswer = readlineSync.question('Your answer: ');
-    const checkForPrime = (randomNum) => {
-      if (randomNumber < 2) {
-        return false;
-      }
-      for (let x = 2; x <= randomNum / 2; x += 1) {
-        if (randomNum % x === 0) {
-          return false;
-        }
-      }
-      return true;
-    };
-    const getCorrectAnswer = (number) => (checkForPrime(number) ? 'yes' : 'no');
-    const correctAnswer = getCorrectAnswer(randomNumber);
-    if (playerAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${userName}.`);
-      return;
+const checkForPrime = (randomNum) => {
+  if (randomNum < 2) {
+    return false;
+  }
+  for (let x = 2; x <= randomNum / 2; x += 1) {
+    if (randomNum % x === 0) {
+      return false;
     }
   }
-  console.log(`Congratulations, ${userName}!`);
+  return true;
+};
+const getCorrectAnswer = (number) => (checkForPrime(number) ? 'yes' : 'no');
+const round = () => {
+  const randomNumber = getRandomInt(1, 99);
+  const question = `Question: ${randomNumber}`;
+  return [getCorrectAnswer(randomNumber), question];
+};
+const primeGame = () => {
+  const task = 'Answer "yes" if the number is prime. Otherwise answer "no".';
+  runGame(round, task);
 };
 
 export default primeGame;
