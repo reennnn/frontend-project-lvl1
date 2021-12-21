58b5b5566344dc3ff41dc8f9078e0a4010a0cd73
rinat <r.bashirov@zenlabs.biz>
2021-12-21 17:33:40 +0500

    fix isPrime function name and some additional changes

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,7 +1,7 @@
 import getRandomInt from '../additional-functions.js';
 import runGame from '../index.js';
 
-const checkForPrime = (randomNum) => {
+const isPrime = (randomNum) => {
   if (randomNum < 2) {
     return false;
   }
@@ -12,15 +12,15 @@ const checkForPrime = (randomNum) => {
   }
   return true;
 };
-const getCorrectAnswer = (number) => (checkForPrime(number) ? 'yes' : 'no');
-const startRound = () => {
+const getCorrectAnswer = (number) => (isPrime(number) ? 'yes' : 'no');
+const prepareRoundData = () => {
   const randomNumber = getRandomInt(1, 99);
   const question = randomNumber;
   return [getCorrectAnswer(randomNumber), question];
 };
 const primeGame = () => {
   const task = 'Answer "yes" if the number is prime. Otherwise answer "no".';
-  runGame(startRound, task);
+  runGame(prepareRoundData, task);
 };
 
 export default primeGame;
